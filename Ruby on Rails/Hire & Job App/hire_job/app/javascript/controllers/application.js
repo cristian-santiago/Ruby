cf28d650baf0f64bdadfcf4b4ee8a205107ca545
cristian-santiago <cristian.santiago.carvalho@gmail.com>
2022-05-27 02:37:53 -0300

    Hire&jobApp
    Add first settings, views and CSS

---
+++ Ruby on Rails/Hire & Job App/hire_job/app/javascript/controllers/application.js
@@ -0,0 +1,19 @@
+import { Application } from "@hotwired/stimulus"
+
+const application = Application.start()
+
+// Configure Stimulus development experience
+application.debug = false
+window.Stimulus   = application
+
+export { application }
+
+
+(() => {
+  'use strict'
+  
+  document.querySelector('#navbarSideCollapse').addEventListener('click', () => {
+    document.querySelector('.offcanvas-collapse').classList.toggle('open')
+  })
+})()
+  
