f61933069065e169280fd5eddddf3844fe9f2312
cristian-santiago <cristian.santiago.carvalho@gmail.com>
2022-06-07 01:55:09 -0300

    adding new controllers

--- Ruby on Rails/Hire & Job App/hire_job/config/routes.rb
+++ Ruby on Rails/Hire & Job App/hire_job/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :applicants, only: [:index,  :new, :create]
+  resources :positions
+  resources :companies, only: [:new, :edit, :update, :create]
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   #get 'home/index'
