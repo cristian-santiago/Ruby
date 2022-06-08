7acdcfa6622d80314d4d292a21078afb6714f828
cristian-santiago <cristian.santiago.carvalho@gmail.com>
2022-06-08 02:30:13 -0300

    Update project, creating edit page

--- Ruby on Rails/Hire & Job App/hire_job/app/controllers/companies_controller.rb
+++ Ruby on Rails/Hire & Job App/hire_job/app/controllers/companies_controller.rb
@@ -4,22 +4,31 @@ class CompaniesController < ApplicationController
   end
 
   def edit
+    @company = current_user.company
+
   end
 
   def create
+
     @company = current_user.build_company(company_params)
     if @company.save
       flash.now[:notice] = 'Company registered successfully.'
       redirect_to root_path
-   else
-    flash.now[:error] = 'Error while register the company.'
-    render :new
-   end
+    else
+      flash.now[:error] = 'Error while register the company.'
+      render :new
+    end
 
   end
 
   def update
 
+    @company = current_user.company
+    if @company.update(company_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   private
--- Ruby on Rails/Hire & Job App/hire_job/app/helpers/companies_helper.rb
+++ Ruby on Rails/Hire & Job App/hire_job/app/helpers/companies_helper.rb
@@ -1,2 +1,9 @@
 module CompaniesHelper
+    def dynamic_url_company
+        if current_user.company.present?
+            edit_company_path(current_user.company)
+        elsif
+            new_company_path
+        end
+    end
 end
