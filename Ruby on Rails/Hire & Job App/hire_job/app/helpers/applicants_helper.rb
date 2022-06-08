e2f517e8de392df9287ed290c018dc04898b7baa
cristian-santiago <cristian.santiago.carvalho@gmail.com>
2022-06-07 21:21:43 -0300

    Update applicants_helper.rb

--- Ruby on Rails/Hire & Job App/hire_job/app/helpers/applicants_helper.rb
+++ Ruby on Rails/Hire & Job App/hire_job/app/helpers/applicants_helper.rb
@@ -1,2 +1,15 @@
 module ApplicantsHelper
+    def flash_msg
+        msg = ""
+        [:notice, :info, :warning, :error].each {|type| 
+        if flash[type]
+        msg += "<p class=\"#{type}\">#{flash[type]}</p>"
+        end
+
+        
+    }
+
+        msg
+    end 
+
 end
