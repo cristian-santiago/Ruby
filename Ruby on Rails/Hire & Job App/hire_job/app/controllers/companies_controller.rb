d97bc1ea9869a18b37d02295f97b2c532b2039bf
cristian-santiago <cristian.santiago.carvalho@gmail.com>
2022-06-07 21:21:36 -0300

    Update companies_controller.rb

--- Ruby on Rails/Hire & Job App/hire_job/app/controllers/companies_controller.rb
+++ Ruby on Rails/Hire & Job App/hire_job/app/controllers/companies_controller.rb
@@ -1,16 +1,31 @@
 class CompaniesController < ApplicationController
   def new
+    @company = current_user.build_company
   end
 
   def edit
   end
 
   def create
+    @company = current_user.build_company(company_params)
+    if @company.save
+      flash.now[:notice] = 'Company registered successfully.'
+      redirect_to root_path
+   else
+    flash.now[:error] = 'Error while register the company.'
+    render :new
+   end
 
   end
 
   def update
 
   end
-  
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :url, :logo)
+  end
+
 end
