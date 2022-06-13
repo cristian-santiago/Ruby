f9b09261408f0738606d184edfc4dfb4fed34c83
cristian-santiago <cristian.santiago.carvalho@gmail.com>
2022-06-13 04:29:06 -0300

    Update 18-Math_table.rb

--- Ruby Exercises/18-Math_table.rb
+++ Ruby Exercises/18-Math_table.rb
@@ -1,6 +1,15 @@
 # Multiplication table using FOR
+def math_table(number)
+    puts "-="*14
+    puts "\tMath Table"
+    puts "-="*14
+    for c in (1..10)
+        puts "\t#{number} x #{c} = #{number*c}"
+    end
+    puts "-="*14
+end
+
 print "Type a number: "
 num = gets.to_i
-for c in [1:11]
-    puts c*num
-end
+
+math_table(num)
