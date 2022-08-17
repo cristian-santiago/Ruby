50cd4860c1475a2f5aa486cf21e6a6c29bb27c06
Cristian Santiago <cristian@cristian.hq.corp.viasat.com>
2022-08-17 07:07:59 -0300

    added deviser user to the app

---
+++ Ruby on Rails/ruby_blog/app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :blogs
+  attr_accessor :username
+
+  validates :username, presence: true
+end
--- Ruby on Rails/ruby_blog/config/routes.rb
+++ Ruby on Rails/ruby_blog/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :all_blogs
   resources :about
   resources :categories
