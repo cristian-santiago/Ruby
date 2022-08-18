2766509ba389fb634d8e18dce4602769a2ae317f
Cristian Santiago <cristian@cristian.hq.corp.viasat.com>
2022-08-18 04:28:10 -0300

    changeg the root page, added style to devise login

--- Ruby on Rails/ruby_blog/app/controllers/all_blogs_controller.rb
+++ Ruby on Rails/ruby_blog/app/controllers/all_blogs_controller.rb
@@ -1,5 +1,5 @@
 class AllBlogsController < ApplicationController
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
   def index
     @users = User.all
     @categories = Category.all
--- Ruby on Rails/ruby_blog/config/routes.rb
+++ Ruby on Rails/ruby_blog/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :all_blogs
+  #resources :all_blogs
   resources :about
   resources :categories
   resources :blogs
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "blogs#index"  
+  root  "all_blogs#index"  
 
 end
 
