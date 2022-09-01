710b8ce477c21990294c583e0941e67a1488cbda
cristian-santiago <cristian.santiago.carvalho@gmail.com>
2022-09-01 20:10:57 -0300

    updated files

--- Ruby on Rails/ruby_blog/test/controllers/all_blogs_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class AllBlogsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get all_blogs_index_url
-    assert_response :success
-  end
-end
